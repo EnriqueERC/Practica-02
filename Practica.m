c4087cb4716b5be75ae0d1808f4c3b9d74fe9f61
EnriqueERC <84213026+EnriqueERC@users.noreply.github.com>
2021-05-17 18:12:28 -0500

    Update Practica.m

--- Practica.m
+++ Practica.m
@@ -119,5 +119,93 @@ xlabel('t'); ylabel('x(2-t)');
 grid;
 axis([-8 8 -1 5]);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Ejercicio 3
+%% 
+% 1.11-1 Provide MATLAB code and output that plots the odd portion xo(t) 
+% of the function x(t) = 2−t cos(2πt)u(t−π ) over a suitable-length interval using 
+% a suitable number of points.
 
+p=pi;
+x=-10:0.1:10;
+f1=(2.^-x).*cos(2*p*x);
+f2=(x<0).*(0)+(x>=0).*(1);
+f3=f1.*(f2-p);
+
+figure
+plot(x,f3,'r')
+title('Grafica ejercicio 1.11-1')
+xlabel('Valores de x')
+ylabel('Valores de f(x)')
+grid on
+ax = gca;
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+
+ax.Box = 'off';
+
+xlim([-10 10])
+ylim([-10 10])
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Ejercicio 4 Version Matlab
+%% 
+% 4. Construye la grafica de para cuatro rangos (figura 2x2). Los ejes deben 
+% mostrarse en el origen además de la edición de su preferencia.
+
+clc 
+clear 
+close all
+p=pi;
+t=-20:0.005:20;
+s=0;
+l=0;
+k=10;
+for n=1:k
+    s=cos(2*p*k*t);
+    l=l+s;
+    plot(t,s,'m')
+    pause(1)
+    title('Grafica original ejercicio 4')
+    xlabel('Valores de x')
+    ylabel('Valores de f(x)')
+    grid on
+    ax = gca;
+    ax.XAxisLocation = 'origin';
+    ax.YAxisLocation = 'origin';
+    xlim([-1.2 1.2])
+    ylim([-1.2 1.2])
+end
+%%
+    
+subplot(1,2,1)
+plot(t,s,'r')
+title('Grafica primer rango')
+xlabel('Valores de x')
+ylabel('Valores de f(x)')
+grid on 
+axis ([-2 2, -2, 2])
+
+
+subplot(1,2,2)
+plot(t,s,'b')
+title('Grafica segundo rango')
+xlabel('Valores de x')
+ylabel('Valores de f(x)')
+grid on 
+axis ([-0.5 0.5, -2.5, 2.5])
+
+subplot(2,1,1)
+plot(t,s,'c')
+title('Grafica tercer rango')
+xlabel('Valores de x')
+ylabel('Valores de f(x)')
+grid on 
+axis ([-7, 7 -2.5 2.5])
+
+subplot(2,1,2)
+plot(t,s,'g')
+title('Grafica cuarto rango')
+xlabel('Valores de x')
+ylabel('Valores de f(x)')
+grid on 
+axis ([-2, 2 -2.2 2.2])
 
