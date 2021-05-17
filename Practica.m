2189bccdfa5395b9dbbf32d4f1f2227117f5f36a
EnriqueERC <84213026+EnriqueERC@users.noreply.github.com>
2021-05-17 18:07:26 -0500

    Update Practica.m

--- Practica.m
+++ Practica.m
@@ -121,23 +121,3 @@ axis([-8 8 -1 5]);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-size(t1)
-size(t2)
-
-t1.*t1;
-
-%%help sinc
-
-%%sinc(t1)
-
-f = @(t) exp(-t).*t+exp(-t).*cos(2*t);
-u = @(t) t>=0;
-p = @(t) (t>=0) & (t<1);
-
-%plot([1,3],[5,-4])
-
-%figure
-%plot(t1,sinc(t1))
-
-figure
-plot(t2,sinc(t2))
